d06e788201da655e872bba740a2cd52cc2fd24d6
Muhammad Taha Raees <raeesmuhammadtaha@gmail.com>
2024-09-24 22:48:06 +0100

    rdp

--- app/games/[gameId]/page.tsx
+++ app/games/[gameId]/page.tsx
@@ -11,7 +11,7 @@ const GameStream: React.FC = () => {
   const videoElementRef = useRef<HTMLVideoElement | null>(null);
 
   useEffect(() => {
-    const ws = new WebSocket('wss://0.tcp.ngrok.io:14763');
+    const ws = new WebSocket('wss://0.tcp.us-cal-1.ngrok.io:14169');
     wsRef.current = ws;
 
     const peer = new Peer({ initiator: false, trickle: false });
